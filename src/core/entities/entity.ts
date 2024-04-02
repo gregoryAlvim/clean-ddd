698a0012418e1372b50755c2b81b916a61ad65b5
Grégory Alvim <gregori.jagua@gmail.com>
2024-04-02 19:02:09 -0300

    add domain events

--- src/core/entities/entity.ts
+++ src/core/entities/entity.ts
@@ -12,4 +12,12 @@ export abstract class Entity<PropsType> {
     this.props = props
     this._id = id ?? new UniqueEntityID()
   }
+
+  public equals(entity: Entity<unknown>) {
+    if (entity === this) return true
+
+    if (entity.id === this._id) return true
+
+    return false
+  }
 }
